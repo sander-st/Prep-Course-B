0adb2bba5fbfb2b61326ba3f032675c3287fa0c1
Sander <code.nodejs.vh@gmail.com>
2022-10-26 18:44:38 -0500

    ejercicio resuelto de 03-JS-III completed

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -4,33 +4,57 @@
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
+const { TaskConfigurationError } = require("simple-git")
+
 function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    let num=0, i=1
+    for( ; i<=10; i++){
+      num += i
+    }
+    return num
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  let pares = []
+  for(let i of array){
+    if((i % 2) === 0){
+      pares.push(i)
+    }
+  }
+  return pares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  let square = array.map(e => e*e)
+  return square
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  let total=0
+  for(let i of array) total+=i
+  
+  return total
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  let digito = `${num}`.length
+  return digito
+
+  // return num.toString().length
 }
   
   
